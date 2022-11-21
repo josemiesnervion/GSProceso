db7ba986fd5dc1933d7eb4e1c4e83a03503405d3
josemiesnervion <josem.estrada@iesnervion.es>
2022-11-21 21:24:48 +0100

    finalizado

--- SincronizacionEstructurasC2E/GestorHojas/src/GestorHojas.java
+++ SincronizacionEstructurasC2E/GestorHojas/src/GestorHojas.java
@@ -1,25 +1,33 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ForkJoinWorkerThread;
+import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
 
 public class GestorHojas extends Thread {
-//Ejercicio sin terminar, Investigar  CopyOnWriteArrayList
-	private static CopyOnWriteArrayList<String> lista = new CopyOnWriteArrayList<String>();//hay que usar este que es thread safe
-
+	private static ConcurrentSkipListSet<String> lista = new ConcurrentSkipListSet<String>();// En principio con este
+																								// funciona
 	@Override
 	public void run() {
 		for (int i = 0; i < 10; i++) {
+
 			lista.add(new String("Texto" + i));
 		}
-
-		for (String string : lista) {
-			System.out.println(string);
+		synchronized (lista) { // tendremos que hacer sincronizada esta parte del codigo para que se
+								// reproduzcan en orden porque sino mezcla los numeros
+			for (String string : lista) {
+				System.out.println(string);
+			}
 		}
 	}
-
 	public static void main(String[] args) {
 		for (int i = 0; i < 10; i++) {
 			new GestorHojas().start();
 		}
-
 	}
-
 }
